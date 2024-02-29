c8163bdb20cd29c7d7793cd8633bbe4568859cbb
Yifan Zhang <yifan2019@icloud.com>
2024-02-29 15:19:28 +0300

    mkdir only once for subjob

--- download_and_split.sh
+++ download_and_split.sh
@@ -2,10 +2,11 @@
 #SBATCH --job-name=download_and_split_$3
 #SBATCH --output=download_and_split/output_task_$3.log
 
+mkdir -p download_and_split_$3
+
 awk "NR >= $1 && NR <= $2" "download_and_split/input.txt" | while read LINE;
 do
   # download wet file
-  mkdir -p download_and_split_$3
   DOWNLOADED="download_and_split_$3/$(basename $LINE)"
   if [ ! -s "$DOWNLOADED" ]; then
     wget -q -O "$DOWNLOADED" "https://data.commoncrawl.org"/${LINE}
