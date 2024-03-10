0d0bd4315ee7cc91b568a64453a87d3d17f75ca5
Yifan Zhang <yifan2019@icloud.com>
2024-03-10 11:58:08 +0300

    remove unfinished job files to allow reprocess

--- download_and_split.sh
+++ download_and_split.sh
@@ -15,6 +15,15 @@ do
   DOWNLOADED="download_and_split_$3/$(basename $LINE)"
   OUTPUT="${DOWNLOADED%gz}pages.jsons.gz"
 
+  # if we have downloaded wet file, we probably didn't finish
+  # previous job, it is better to remove both output file and
+  # redo this particular file
+  if [ -s "$DOWNLOADED" ]; then
+    rm -f "$OUTPUT"
+    rm -f "$DOWNLOADED"
+  fi
+
+  # if output file exist, skip; if not, download and split
   if [ ! -s "$OUTPUT" ]; then
     if [ ! -s "$DOWNLOADED" ]; then
       while true; do
@@ -23,6 +32,8 @@ do
     fi
     python3 split_wet_file.py "$DOWNLOADED"
   fi
+
+  # remove downloaded wet file to save space, only keep output
   if [ -s "$OUTPUT" ]; then
     rm -f "$DOWNLOADED"
   fi
