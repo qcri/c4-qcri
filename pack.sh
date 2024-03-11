b2c4cfc35aded1a71c3877ec1792cb5bae684eec
Yifan Zhang <yifan2019@icloud.com>
2024-03-11 12:06:34 +0300

    fix names

--- pack.sh
+++ pack.sh
@@ -8,6 +8,8 @@ LINES_PER_TASK=$((NUM_LINES / NUM_TASKS))
 EXTRA_LINES=$((NUM_LINES % NUM_TASKS))
 INPUT_FILE="download_and_split/input.txt"
 
+rm -f download_and_split/output.txt
+
 for ((i = 0; i < NUM_TASKS; i++)); do
     START_LINE=$((i*LINES_PER_TASK + 1))
     END_LINE=$((START_LINE + LINES_PER_TASK - 1))
@@ -15,14 +17,12 @@ for ((i = 0; i < NUM_TASKS; i++)); do
         END_LINE=$((END_LINE + EXTRA_LINES))
     fi
 
-    rm -f download_and_split/output.txt
-
-    awk "NR >= $1 && NR <= $2" "download_and_split/input.txt" | \
+    awk "NR >= START_LINE && NR <= END_LINE" "download_and_split/input.txt" | \
       while read LINE;
     do
 
       # download wet file
-      DOWNLOADED="download_and_split_$3/$(basename $LINE)"
+      DOWNLOADED="download_and_split_$i/$(basename $LINE)"
       OUTPUT="${DOWNLOADED%gz}pages.jsons.gz"
 
       echo $OUTPUT >> download_and_split/output.txt
@@ -31,6 +31,7 @@ done
 
 cat "download_and_split/output.txt" | while read LINE; do
   # group split into a large gz file
-  GZFILENAME=${$(basename LINE)%-*}
+  BASENAME=$(basename $LINE)
+  GZFILENAME=${BASENAME%-*}
   cat $LINE >> $GZFILENAME.warc.wet.pages.jsonl.gz
 done
