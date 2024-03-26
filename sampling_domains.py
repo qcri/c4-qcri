f5292baa1a669d369815a0fdc9a3d7286bee4617
Yifan Zhang <yifan2019@icloud.com>
2024-03-26 15:06:31 +0300

    select only samples whose main language is Arabic

--- sampling_domains.py
+++ sampling_domains.py
@@ -19,14 +19,17 @@ samples = defaultdict(list)
 
 for i, line in enumerate(fileinput.input(files=("-"), encoding="utf-8")):
   page = json.loads(line)
+
   # in order to process the result of our script (merge results), we
   # will use domain and domain_count if available
   domain = page.get('domain', domain_of(page['url']))
   count = page.get('domain_count', 1)
   domains.update({domain: count})
-  if len(samples[domain]) < NSAMPLES:
+
+  if len(samples[domain]) < NSAMPLES and page['language'].startswith("ara"):
     samples[domain].append({'domain': domain, **page})
 
 for domain, count in domains.most_common():
-  for page in samples[domain]:
-    print(json.dumps({'domain_count': count, **page}, ensure_ascii=False), file=sys.stdout)
+  for page in samples.get(domain, []):
+    page.update({'domain_count': count})
+    print(json.dumps(page, ensure_ascii=False), file=sys.stdout)
